2209ddd856c8a59f862c1c076d05b9ef74dcdf1c
Cao Lê Gia Phú <23127535@student.hcmus.edu.vn>
2024-12-22 14:17:30 +0700

    final

---
+++ SourceCode Keygen/2.3.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Point {
+  unsigned int x;
+  unsigned int y;
+};
+
+unsigned int t1 = 0x00, t2 = 0x00;
+string Name, serial;
+unsigned int Namelen, SizeMaze;
+string Maze;
+Point start, finish;
+string key;
+
+void Init() {
+  for (int i = 3; i >= 0; i--) {
+    char s = Name[i] & 0xff;
+    t1 = t1 << 8;
+    t1 = t1 + int(s);
+  }
+  Namelen = Name.length();
+  for (int i = Namelen - 1; i >= 4; i--) {
+    char s = Name[i] & 0xff;
+    t2 = t2 << 8;
+    t2 = t2 + int(s);
+  }
+}
+
+int RandomNumber(unsigned int num) {
+  unsigned int a = t1;
+  unsigned int d = 0x5dc1;
+  a = a * d;
+  unsigned int c = 0x560b;
+  d = 0;
+  t1 = a % c;
+  a = t2;
+  d = 0x549d;
+  a = a * d;
+  c = 0x51a1;
+  d = 0;
+  t2 = a % c;
+  a = t1;
+  d = t2;
+  a = a + d;
+  c = num;
+  unsigned int b = a % c;
+  a = b;
+  return a;
+}
+
+void CreateMaze() {
+  unsigned int num = 0x14;
+  unsigned int a = RandomNumber(num);
+  a += 0x14;
+  SizeMaze = a;
+  unsigned int b = 0;
+  while (b < SizeMaze * SizeMaze) {
+    num = 0x4;
+    a = RandomNumber(num);
+    if (a == 0)
+      Maze += "=";
+    else
+      Maze += ".";
+    b++;
+  }
+
+  num = SizeMaze;
+  start.x = RandomNumber(num);
+  start.y = RandomNumber(num);
+
+  do {
+    num = SizeMaze;
+    finish.x = RandomNumber(num);
+  } while (start.x == finish.x);
+
+  do {
+    num = SizeMaze;
+    finish.y = RandomNumber(num);
+  } while (start.y == finish.y);
+
+  a = start.x;
+  unsigned int d = SizeMaze;
+  a = a * d;
+  d = start.y;
+  a += d;
+  Maze[a] = 'S';
+
+  a = finish.x;
+  d = SizeMaze;
+  a = a * d;
+  d = finish.y;
+  a += d;
+  Maze[a] = 'F';
+}
+
+bool SolveMaze(int x, int y) {
+  if (x > SizeMaze - 1 || x < 0 || y > SizeMaze - 1 || y < 0)
+    return false;
+  if (Maze[x * SizeMaze + y] == 'F')
+    return true;
+  if (Maze[x * SizeMaze + y] != '.' && Maze[x * SizeMaze + y] != 'S')
+    return false;
+  Maze[x * SizeMaze + y] = '+';
+
+  if (SolveMaze(x - 1, y) == true) {
+    key += 'l';
+    return true;
+  }
+  if (SolveMaze(x + 1, y) == true) {
+    key += 'r';
+    return true;
+  }
+  if (SolveMaze(x, y + 1) == true) {
+    key += 'd';
+    return true;
+  }
+  if (SolveMaze(x, y - 1) == true) {
+    key += 'u';
+    return true;
+  }
+  Maze[x * SizeMaze + y] = ' ';
+  return false;
+}
+
+string GetKey() {
+  if (SolveMaze(start.x, start.y)) {
+    string temp = "";
+    for (int i = key.length() - 1; i >= 0; i--) {
+      temp += key[i];
+    }
+    return temp;
+  } else {
+    return "Unsolvable";
+  }
+}
+
+void Printmaze() // In ma trận ra màn hình(không cần thiết)
+{
+  for (int i = 0; i < SizeMaze; i++) {
+    for (int j = 0; j < SizeMaze; j++)
+      cout << Maze[i * SizeMaze + j];
+    cout << endl;
+  }
+}
+
+int main(void) {
+
+  do {
+    cout << "UserName(5 -> 8 char): ";
+    cin >> Name;
+  } while (Name.length() < 5 || Name.length() > 8);
+
+  Init();
+  CreateMaze();
+  Printmaze();
+  cout << endl;
+  cout << "ValidKey: " << GetKey() << endl;
+  return 0;
+}
---
+++ SourceCode Keygen/2.5(macos).cpp
@@ -0,0 +1,242 @@
+﻿#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
+
+using namespace std;
+
+string LengthString(string s) {
+    if (s.empty())
+        return "";
+    return string(1, s.back());
+}
+
+void clearScreen() {
+    cout << "\033[2J\033[H";
+}
+
+void Out(char a[16][16], string s) {
+    clearScreen();
+    cout << "NAME :" << s << endl;
+    cout << "HOW TO GET SERIAL : 9 is Snake - C is Apple - D is Destination" << endl;
+    cout << "Move Snake to eat all Apple(C) and then move to Destination (D)" << endl;
+    cout << "NOTE : This code isn't incomplete." << endl;
+    cout << "1. Moving snake reasonably to get SERIAL correctly." << endl;
+    cout << "2. Avoid moving snake circles." << endl << endl;
+    cout << "HOW TO MOVE :" << endl;
+    cout << "W - UP" << endl;
+    cout << "S - DOWN" << endl;
+    cout << "A - LEFT" << endl;
+    cout << "D - RIGHT" << endl << endl;
+
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            if (a[i][j] == 'C') {
+                cout << "\033[32m" << a[i][j] << "\033[0m  ";
+            } else if (a[i][j] == 'D') {
+                cout << "\033[31m" << a[i][j] << "\033[0m  ";
+            } else if (a[i][j] == '9') {
+                cout << "\033[33m" << a[i][j] << "\033[0m  ";
+            } else {
+                cout << a[i][j] << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void Init(char a[16][16]) {
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < 16; j++)
+            a[i][j] = '0';
+}
+
+void ShowKey(string &s) {
+    while (s.find("0000") != string::npos)
+        s.replace(s.find("0000"), 4, "C");
+    while (s.find("1111") != string::npos)
+        s.replace(s.find("1111"), 4, "D");
+    while (s.find("2222") != string::npos)
+        s.replace(s.find("2222"), 4, "E");
+    while (s.find("3333") != string::npos)
+        s.replace(s.find("3333"), 4, "F");
+}
+
+void ShowKey2(string &s) {
+    while (s.find("000") != string::npos)
+        s.replace(s.find("000"), 3, "8");
+    while (s.find("111") != string::npos)
+        s.replace(s.find("111"), 3, "9");
+    while (s.find("222") != string::npos)
+        s.replace(s.find("222"), 3, "A");
+    while (s.find("333") != string::npos)
+        s.replace(s.find("333"), 3, "B");
+}
+
+void ShowKey3(string &s) {
+    while (s.find("00") != string::npos)
+        s.replace(s.find("00"), 2, "4");
+    while (s.find("11") != string::npos)
+        s.replace(s.find("11"), 2, "5");
+    while (s.find("22") != string::npos)
+        s.replace(s.find("22"), 2, "6");
+    while (s.find("33") != string::npos)
+        s.replace(s.find("33"), 2, "7");
+}
+
+int EnteredName(char a[16][16], string s, int &rsour, int &csour, int &rdes, int &cdes) {
+    int dl = 0;
+    int length = s.length();
+    int div, mod;
+    int rapple[20], capple[20];
+    
+    for (int i = 0; i < length; i++) {
+        dl += s[i];
+    }
+
+    int temp;
+    for (int i = 0; i < length; i++) {
+        temp = int(s[i]) ^ dl;
+        dl -= temp;
+        if (dl < 0) dl += 256;
+        div = (temp % 256) / 16;
+        mod = (temp % 256) % 16;
+        if (a[div][mod] == '0')
+            a[div][mod] = 'C';
+        else
+            return 0;
+        rapple[i] = div;
+        capple[i] = mod;
+    }
+
+    dl ^= temp;
+    temp -= dl;
+    if (temp < 0) temp += 256;
+    div = (temp % 256) / 16;
+    mod = (temp % 256) % 16;
+    if (a[div][mod] == '0')
+        a[div][mod] = 'D';
+    else
+        return 0;
+    rdes = div;
+    cdes = mod;
+
+    temp = dl;
+    div = (temp % 256) / 16;
+    mod = (temp % 256) % 16;
+    if (a[div][mod] == '0')
+        a[div][mod] = '9';
+    else
+        return 0;
+    rsour = div;
+    csour = mod;
+
+    return 1;
+}
+
+int kbhit(void) {
+    struct timeval tv = {0, 0};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+}
+
+char getch() {
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~(ICANON | ECHO);
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= (ICANON | ECHO);
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    return buf;
+}
+
+int main() {
+    char a[16][16];
+    int rsour, csour, rdes, cdes;
+    string s = " ", name;
+    Init(a);
+
+    while (true) {
+        cout << "NAME :";
+        getline(cin, name);
+        cout << endl << endl;
+        if (EnteredName(a, name, rsour, csour, rdes, cdes)) {
+            Out(a, name);
+            break;
+        }
+    }
+
+    int k = 1;
+    while (k) {
+        if (kbhit()) {
+            char ch = getch();
+            switch (tolower(ch)) {
+            case 'w':
+                if (LengthString(s) == "0")
+                    break;
+                a[rsour][csour] = '0';
+                if (rsour - 1 < 0)
+                    break;
+                rsour--;
+                a[rsour][csour] = '9';
+                s += "1";
+                Out(a, name);
+                break;
+            case 's':
+                if (LengthString(s) == "1")
+                    break;
+                a[rsour][csour] = '0';
+                if (rsour + 1 > 15)
+                    break;
+                rsour++;
+                a[rsour][csour] = '9';
+                s += "0";
+                Out(a, name);
+                break;
+            case 'a':
+                if (LengthString(s) == "3")
+                    break;
+                a[rsour][csour] = '0';
+                if (csour - 1 < 0)
+                    break;
+                csour--;
+                a[rsour][csour] = '9';
+                s += "2";
+                Out(a, name);
+                break;
+            case 'd':
+                if (LengthString(s) == "2")
+                    break;
+                a[rsour][csour] = '0';
+                if (csour + 1 > 15)
+                    break;
+                csour++;
+                a[rsour][csour] = '9';
+                s += "3";
+                Out(a, name);
+                break;
+            }
+            if (csour == cdes && rsour == rdes) {
+                k = 0;
+            }
+        }
+    }
+
+    cout << "Move :" << s << endl;
+    ShowKey(s);
+    ShowKey2(s);
+    ShowKey3(s);
+    cout << "SERIAL is :" << s << endl;
+    cout << "Press Enter to exit..." << endl;
+    cin.get();
+    return 0;
+}
